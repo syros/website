7971337bfb6e6d35755547a0485f989d0caca9ae
Larisa Markova <lovemirror@rambler.ru>
2024-06-03 17:26:35 +0300

    Add packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svelte-svgr';
+import { sveltrisVitePlugins } from 'sveltris';
 
 export default defineConfig({
   // site: 'https://AdmiralDS.github.io',
   // base: '/website/',
   // output: 'static',
   // base: ''
-  plugins: [sveltekit(), svgr()],
+  plugins: [sveltekit(), svgr(), sveltrisVitePlugins()],
 });
