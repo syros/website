a0519c2c47c2271110a50ce5af12fbb4b1cbf028
Larisa Markova <lovemirror@rambler.ru>
2024-06-03 12:40:55 +0300

    Add Chips

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,6 @@
 export * from './Tag';
 export * from './Toggle';
 export * from './Button';
+export * from './Chips';
 export * from './icons';
 export { default as Divider } from './Divider.svelte';
