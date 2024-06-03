f2d3ba02766e7c70a1154ca08e3fec7d6e7d9d2e
Anna Danshina <annetteblackat@gmail.com>
2024-06-03 10:23:03 +0300

    перевела страницу пользователей

---
+++ src/components/pageComponents/users/Content/UserList/UserList.svelte
@@ -0,0 +1,21 @@
+<script>
+    import users from './users.json';
+    import { UserCard } from '../UserCard';
+</script>
+
+<div class="users-list">
+    {#each users as user}
+        <UserCard index={user.id} {...user} />
+    {/each}
+</div>
+
+<style>
+.users-list {
+    display: flex;
+    flex-direction: column;
+    padding-top: 20px;
+    padding-bottom: 100px;
+    border-bottom: 1px solid var(--Light_grey, #e6eaf0);
+    gap: 10px;
+}
+</style>
--- src/components/pageComponents/users/index.ts
+++ src/components/pageComponents/users/index.ts
@@ -1,2 +1,2 @@
 export * from './Header';
-// export * from './content';
+export * from './Content';
